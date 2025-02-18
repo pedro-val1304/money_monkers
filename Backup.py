e8db4ef0fdcddcc58efad4caaafb00171947da9c
EBLopex <emiliano.becerra1485@gmail.com>
2025-02-18 13:46:06 -0600

    Update Backup.py

--- Backup.py
+++ Backup.py
@@ -1,14 +1,15 @@
-surList = ["Becerra", "Facio", "Jimenez", "Rodriguez", "Gutierrez", "Valenzuela", "Vidrio", "Mora", "Valdivia", "Ibarra", "Ornelas", "Zepeda", "Granados", "Alvarez"]
-codeList = ["224164515", "219439208", "220520973", "221925365", "221453145", "221568619", "224164513", "221964573", "216548448", "224164491", " 218466716", "220571586", " 218220776","221851264"]
-nameList = ["Emiliano", "Allison","Naomi","Natalia","Pedro","Jose","Diego", "Abril","Grecia","Misael","Alberto","Elias","Angel", "Gloria"]
-pList = [80, 90] #Programación
-fList = [60, 100] #Física
+nameList = ["Emiliano", "Allison", "Naomi", "Natalia", "Pedro", "Jose", "Diego", "Abril", "Grecia", "Misale", "Alberto", "Elias", "Angel"]
+surList = ["Becerra", "Facio", "Jimenez", "Rodriguez", "Gutierrez", "Valenzuela", "Vidrio", "Mora", "Valdivia", "Ibarra", "Ornelas", "Zepeda", "Granados"]
+codeList = ["224164515", "219439208", "220520973", "221925365", "221453145", "221568619", "224164513", "221964573", "216548448", "224164491", " 218466716", "220571586", " 218220776"]
+pList = [80, 90, 90, 70, 50, 100, 85, 95, 77, 98, 50, 65, 79, 100] #Programación
+fList = [60, 100, 70, 80, 83, 100, 94, 65, 97, 85, 100, 90, 100, 100 ] #Física
 qList = [70, 80] #Química
-cList = [90, 70] #CB
+cList = [90, 70, 95, 73, 90, 100, 55, 69, 100, 100, 84, 89, 88, 90] #CB
 asisList = [15, 16] #Asistencia
 absList = [2, 1] #Faltas
-ageList = [19, 20] #Edad
+ageList = [19, 20, 22, 18, 18, 18, 19, 18, 24, 18, 21, 19, 22, 18] #Edad
 carList = ["LTBI", "LTBI"] #Carrera
+# alberto-ornelas-patch-1
 
 borrar = str
 caP = float
