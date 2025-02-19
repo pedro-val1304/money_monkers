6a4824959673af956982d153019c59d000505cef
pedro-val1304 <jose.valenzuela6861@alumnos.udg.mx>
2025-02-18 23:28:28 -0600

    Update money_monkers.py

--- money_monkers.py
+++ money_monkers.py
@@ -1,6 +1,6 @@
 
 nameList = ["Emiliano", "Allison", "Naomi", "Natalia", "Pedro", "Jose", "Diego", "Abril", "Grecia", "Misale", "Alberto", "Elias", "Angel"]
-surList = ["Becerra", "Facio", "Jimenez", "Rodriguez", "Gutierrez", "Valenzuela", "Vidrio", "Mora", "Valdivia", "Ibarra", "Ornelas", "Zepeda", "Granados"]
+surList = ["BECERRA", "FACIO", "JIMENEZ", "RODRIGUEZ", "GUTIERREZ", "VALENZUELA", "VIDRIO", "MORA", "VALDIVIA", "IBARRA", "ORNELAS", "ZEPEDA", "GRANADOS", "ALVAREZ"]
 codeList = ["224164515", "219439208", "220520973", "221925365", "221453145", "221568619", "224164513", "221964573", "216548448", "224164491", " 218466716", "220571586", " 218220776"]
 pList = [80, 90, 90, 70, 50, 100, 85, 95, 77, 98, 50, 65, 79, 100] #Programación
 fList = [60, 100, 70, 80, 83, 100, 94, 65, 97, 85, 100, 90, 100, 100 ] #Física
